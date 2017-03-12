cc7b303279bb15b8a5ee8cbe60fc3a2c893f95b4
silverwedding <dan_e12@hotmail.com>
2017-03-12 13:40:07 +0000

    change webpack config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -39,8 +39,33 @@ module.exports = {
             "React": "react"
         }),
         new webpack.DefinePlugin({
-		    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+		  'process.env': {
+		    NODE_ENV: JSON.stringify('production')
+		  }
 		}),
-		new webpack.optimize.CommonsChunkPlugin('common.js')
+		new webpack.optimize.CommonsChunkPlugin('common.js'),
+ 		new webpack.optimize.DedupePlugin(),
+ 		new webpack.optimize.AggressiveMergingPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+	      mangle: true,
+	      compress: {
+	        warnings: false, // Suppress uglification warnings
+	        pure_getters: true,
+	        unsafe: true,
+	        unsafe_comps: true,
+	        screw_ie8: true
+	      },
+	      output: {
+	        comments: false,
+	      },
+	      exclude: [/\.min\.js$/gi] // skip pre-minified libs
+	    }),
+	    new CompressionPlugin({
+	      asset: "[path].gz[query]",
+	      algorithm: "gzip",
+	      test: /\.js$|\.css$|\.html$/,
+	      threshold: 10240,
+	      minRatio: 0
+	    })
     ]
 };
